7bf156e1806a37a56bfe973c098b82bad666940f
eduramirez87 <edu.ramirez645@gmail.com>
2022-10-11 09:39:19 -0300

    backend

---
+++ BackEnd/src/main/java/com/portfolio/emr/EmrApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.emr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmrApplication.class, args);
+	}
+
+}
