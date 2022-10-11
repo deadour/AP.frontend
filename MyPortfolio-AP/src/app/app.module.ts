4575eb2122074200c036aac83e7578b4bf983e5b
eduramirez87 <edu.ramirez645@gmail.com>
2022-10-11 00:34:21 -0300

    header

--- MyPortfolio-AP/src/app/app.module.ts
+++ MyPortfolio-AP/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { SocialComponent } from './components/social/social.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    SocialComponent
   ],
   imports: [
     BrowserModule,
