b8ac1aa696e1243f397645cc7840afb96c12bbb8
eduramirez87 <edu.ramirez645@gmail.com>
2022-10-11 04:23:33 -0300

    acerca de mi
    agregado bootstrap, header, banner, acerca de mi.

--- MyPortfolio-AP/src/app/app.module.ts
+++ MyPortfolio-AP/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { BannerComponent } from './components/banner/banner.component';
     HeaderComponent,
     LogoAPComponent,
     SocialComponent,
-    BannerComponent
+    BannerComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
